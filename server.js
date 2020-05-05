6a8fa18c1e021f2a30c935c6197a02687718f7d6
Glitch (slow-veiled-prawn) <none>
2020-05-05 17:20:58 +0000

    🐣🛎 Updated with Glitch

--- server.js
+++ server.js
@@ -3,32 +3,48 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const express = require('express');
 const app = express();
+//bodyParser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
+const todos = [
+  'Đi chợ',
+  'Nấu cơm',
+  'Rửa chén',
+  'Học code tại CoderX'
 ];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+// Pug template engine
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get('/', (request, response) => {
+  response.send('I love CodersX');
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+//get todo
+app.get('/todos', (req, res) => {
+  let q = req.query.q;
+  let match = q && todos.filter(element => 
+    element.toLowerCase().indexOf(q.toLowerCase()) !== -1? true: false                          
+  );
+  res.render('./todos', {
+    todos: match||todos
+  });
 });
-
+//create todo
+app.post('/todos/create', (req, res) => {
+    let todo = req.body.todo;
+  console.log(todo);
+    if (todo) {
+      todos.push(todo);
+    }
+    res.redirect('/todos');
+});
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+app.listen(process.env.PORT, () => {
+  console.log("Server listening on port " + process.env.PORT);
 });
